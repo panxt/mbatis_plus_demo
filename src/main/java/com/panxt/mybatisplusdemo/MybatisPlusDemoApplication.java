56d82a18798138e261985561be5bb449bb676601
panxt <15639727428@163.com>
2020-04-05 05:15:56 +0800

    init project

---
+++ src/main/java/com/panxt/mybatisplusdemo/MybatisPlusDemoApplication.java
@@ -0,0 +1,16 @@
+package com.panxt.mybatisplusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 15639
+ */
+@SpringBootApplication
+public class MybatisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemoApplication.class, args);
+    }
+
+}
